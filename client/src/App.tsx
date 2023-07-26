624f3817dd72069c4aa91abc84c6f6c45eb1a6a7
pvwnthem <pvwn.them@gmail.com>
2023-07-26 02:00:50 -0400

    start adding some styling and a title on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react"
+import Title from "./components/Title"
+
+import './App.css'
 
 function App() {
 
   return (
     <>
+      <div className="w-screen h-screen background">
+        <div className="flex w-full h-full items-center justify-center">
+          <Title />
+        </div>
+      </div>
+      
     </>
   )
 }
