6f5b79993669c0449164c2bdb4226bb29b250b03
pvwnthem <pvwn.them@gmail.com>
2023-07-26 01:38:40 -0400

    add cors to server for local development

--- server/main.go
+++ server/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
