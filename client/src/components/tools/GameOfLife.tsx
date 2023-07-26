a1b38b2e074da19df2fb6d1a9a95a45ca44c666d
pvwnthem <pvwn.them@gmail.com>
2023-07-26 18:33:58 -0400

    improvements

--- client/src/components/tools/GameOfLife.tsx
+++ client/src/components/tools/GameOfLife.tsx
@@ -1,62 +1,70 @@
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
+
+const GRID_SIZE = 10;
+const INITIAL_FILLED_CELLS = 20;
 
 const GameOfLife = () => {
   const [grid, setGrid] = useState<number[][]>([]);
 
- useEffect(() => {
-  // Initialize the grid with the initial state (e.g., all cells are dead).
-  const initialGrid = Array.from({ length: 10 }, () => Array(10).fill(0));
+  useEffect(() => {
+    // Initialize the grid with the initial state (e.g., all cells are dead).
+    const initialGrid = Array.from({ length: GRID_SIZE }, () => Array(GRID_SIZE).fill(0));
 
-  // Randomly fill in some squares on the initial grid.
-  for (let i = 0; i < 20; i++) {
-    const row = Math.floor(Math.random() * 10);
-    const col = Math.floor(Math.random() * 10);
-    initialGrid[row][col] = 1;
-  }
+    // Randomly fill in some squares on the initial grid.
+    for (let i = 0; i < INITIAL_FILLED_CELLS; i++) {
+      const row = Math.floor(Math.random() * GRID_SIZE);
+      const col = Math.floor(Math.random() * GRID_SIZE);
+      initialGrid[row][col] = 1;
+    }
 
-  setGrid(initialGrid);
-}, []);
+    setGrid(initialGrid);
+  }, []);
 
-  const handleNextGeneration = async () => {
+  const handleNextGeneration = useCallback(async () => {
     try {
       // Make an API call to get the next generation by passing the current grid
-      const response = await axios.post('http://localhost:8080/tools/game-of-life/next', { grid });
+      const response = await axios.post('http://localhost:8080/tools/game-of-life/next', {
+        grid,
+      });
       setGrid(response.data.grid);
     } catch (error) {
       console.error('Error fetching next generation:', error);
+      // Show user-friendly error message to the user
+      // setError('An error occurred while fetching the next generation.');
     }
-  };
+  }, [grid]);
 
-  // Function to toggle a cell's state when clicked
-  const toggleCell = (rowIndex: number, colIndex: number) => {
+  const toggleCell = useCallback((rowIndex: number, colIndex: number) => {
     const updatedGrid = [...grid];
     updatedGrid[rowIndex][colIndex] = 1 - updatedGrid[rowIndex][colIndex];
     setGrid(updatedGrid);
+  }, [grid]);
+
+  const handleReset = () => {
+    const initialGrid = Array.from({ length: GRID_SIZE }, () => Array(GRID_SIZE).fill(0));
+    setGrid(initialGrid);
   };
 
   return (
-    <div>
-      <div className="grid grid-cols-10 gap-1">
+    <div className='w-screen h-screen'>
+      <div className='grid grid-cols-10 gap-1' style={{ width: '200px', height: '200px' }}>
         {/* Render the grid */}
         {grid.map((row, rowIndex) =>
           row.map((cell, colIndex) => (
             <div
               key={`${rowIndex}-${colIndex}`}
-              className={`w-8 h-8 ${
-                cell === 1 ? 'bg-black' : 'bg-white'
-              }`}
+              className={`w-full h-full border border-gray-300 ${cell === 1 ? 'bg-black' : 'bg-white'}`}
               onClick={() => toggleCell(rowIndex, colIndex)}
             ></div>
           ))
         )}
       </div>
-      <button
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-        onClick={handleNextGeneration}
-      >
+      <button className='mt-4 mr-4 px-4 py-2 bg-blue-500 text-white rounded' onClick={handleNextGeneration}>
         Next Generation
+      </button>
+      <button className='mt-4 px-4 py-2 bg-gray-500 text-white rounded' onClick={handleReset}>
+        Reset
       </button>
     </div>
   );
