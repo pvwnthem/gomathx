7d97ff5b8cc5956a7aaa3fa068c19f49bd90cba1
pvwnthem <pvwn.them@gmail.com>
2023-07-26 17:26:22 -0400

    remove test tool

--- client/src/constants/tools.ts
+++ client/src/constants/tools.ts
@@ -1,10 +1,4 @@
 import { Tool } from "../types/Tool";
 
 export const tools: Tool[] = [
-    {
-        name: "Quadratics",
-        description: "A tool to help you solve and experiment with quadratic equations.",
-        href: "/tools/quadratics",
-        image: "/images/tools/quadratics.png"
-    }
 ]
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,14 +5,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import Quadratics from './components/tools/Quadratics';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
         <Routes>
             <Route path="/" Component={App} />
-            <Route path="/tools/quadratics" Component={Quadratics} />
         </Routes>
     </Router>
   </React.StrictMode>,
