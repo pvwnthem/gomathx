0f0e688232a09022e807c5b0d8adf6cfc6ce8e28
pvwnthem <pvwn.them@gmail.com>
2023-07-27 00:07:58 -0400

    better toolbox styling

--- client/src/components/ToolBox.tsx
+++ client/src/components/ToolBox.tsx
@@ -5,10 +5,10 @@ function ToolBox({ tool }: { tool: Tool }) {
   return (
     <a
       href={tool.href}
-      className="border rounded-lg p-4 shadow-md w-full mx-auto bg-white dark:bg-gray-800 dark:text-white hover:shadow-lg transition duration-300"
+      className="border rounded-lg p-4 shadow-md w-full mx-auto background hover:shadow-lg transition duration-300"
     >
-      <h2 className="text-center text-lg font-semibold mb-2">{tool.name}</h2>
-      <p className="text-sm mb-4">{tool.description}</p>     
+      <h2 className="text-center text-lg font-semibold mb-2 go">{tool.name}</h2>
+      <p className="text-sm mb-4 text-white">{tool.description}</p>     
       <div className="mt-2">
         <img
           src={tool.image}
